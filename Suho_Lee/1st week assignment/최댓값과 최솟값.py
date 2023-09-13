11c8295b9588718a0bbc6f7a6f2b1f2b441fd677
DESKTOP-HMR0FSF\이수호 <phrabit@gmail.com>
2023-09-14 00:30:49 +0900

    FirstDay_0914

---
+++ Suho_Lee/1st week assignment/최댓값과 최솟값.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = ""
+
+    # 문자열 분리를 위한 split
+    # list안의 원소들을 int형으로 바꾸기 위한 map
+    # 정렬 sorted 이후, 최댓값 li[0], 최소값 li[-1] 사용.
+    li = sorted(list(map(int, s.split(" "))))
+    answer += (str(li[0]) + " " + str(li[-1]))
+
+    return answer
