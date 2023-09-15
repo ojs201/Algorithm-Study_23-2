8d88196146d7b84a2f3ca15803aa6d344969adcd
onyourYujin <122829071+onyourYujin@users.noreply.github.com>
2023-09-16 01:40:32 +0200

    Revert "yujin"

--- yujin/1st week assignment/Jadencase 문자 만들기.py
+++
@@ -1,6 +0,0 @@
-def solution(s):
-    words = s.split(" ")
-    new_words = []
-    for word in words:
-        new_words.append(word.capitalize())
-    return ' '.join(new_words)
--- yujin/1st week assignment/최댓값과 최솟값.py
+++
@@ -1,5 +0,0 @@
-def solution(s):
-    lst = list(map(int,s.split( )))
-    lst.sort()
-    answer = f"{lst[0]} {lst[-1]}"
-    return answer
