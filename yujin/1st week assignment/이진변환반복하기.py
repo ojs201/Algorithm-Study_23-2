9bc855d75c324d15a0630d9225d18cd62514c3c6
onyourYujin <ys991120@gmail.com>
2023-09-19 11:22:34 +0200

    불필요한 코드 삭제

--- yujin/1st week assignment/이진변환반복하기.py
+++ yujin/1st week assignment/이진변환반복하기.py
@@ -1,11 +1,9 @@
 def solution(s):
     zero = 0
     cnt = 0
-    for _ in range(len(s)):
+    while s != "1":
         zero += s.count("0")
         s = s.replace("0","")
-        s = bin(len(s))[2:]
+        s = bin(len(s))[2:] # bin : 이진 표현을 나타내는 접두사 '0b'로 시작
         cnt += 1
-        if s == '1':
-            break
     return cnt, zero
