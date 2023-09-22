2fb3728109877c8ced61e133b8999d0b8f488d39
ojs201 <jaesukoh@naver.com>
2023-09-23 03:35:33 +0900

    2주차 과제 1차제출

--- Omega/1st week assignment/숫자의 표현.py
+++ Omega/1st week assignment/숫자의 표현.py
@@ -1,6 +1,7 @@
 # 숫자의 표현
 # https://school.programmers.co.kr/learn/courses/30/lessons/12924
 
+"""
 n = int(input())
 cnt = 0
 k = int((n / 2) + 1)
@@ -14,4 +15,21 @@ for i in range(1, k):
             cnt += 1
             break
 print(cnt)
+"""
 
+
+def solution(n):
+    cnt = 0
+    k = int((n // 2) + 1)
+
+    for i in range(1, k):
+        for j in range(i + 1, n):
+            i += j
+            
+            if i > n:
+                break
+            elif i == n:
+                cnt += 1
+                break
+    
+    return cnt + 1
