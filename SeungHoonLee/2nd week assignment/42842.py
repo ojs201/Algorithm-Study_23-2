09d424128d4f9da43ebe6fda3313685280368afb
SeungHoon Lee <lsh901673@gmail.com>
2023-09-24 14:34:56 +0900

    refactor: 카펫(42842) 수정
    반복문의 시작을 2에서 3으로 변경: 
    
    - 문제에서 주어진 yellow는 1 이상인 자연수이다.
    - 반복문의 시작이 2인 경우 yellow가 0인 경우를 포함한다.

--- SeungHoonLee/2nd week assignment/42842.py
+++ SeungHoonLee/2nd week assignment/42842.py
@@ -36,7 +36,7 @@ from math import sqrt, ceil
 def solution1(brown, yellow):
     br = brown + yellow
 
-    for w in range(2, ceil(sqrt(br)) + 1):
+    for w in range(3, ceil(sqrt(br)) + 1):
         h = br // w
 
         if (w - 2) * (h - 2) == yellow:
