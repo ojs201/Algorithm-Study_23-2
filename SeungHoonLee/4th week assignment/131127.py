25b61c4e080f84bc07bc063e428a18a89d789ebd
LEE-sh1673 <lsh901673@gmail.com>
2023-10-06 16:02:47 +0900

    refactor: 할인 행사(131127) 메서드 삭제
    - 필요없는 helper 메서드 삭제

--- SeungHoonLee/4th week assignment/131127.py
+++ SeungHoonLee/4th week assignment/131127.py
@@ -13,12 +13,8 @@ LV2. 할인 행사
 
 
 def solution(want, number, discount) -> bool:
-    return sum(is_products_discounted(want, number, discount[day:day + 10]) for day in range(len(discount)))
+    return sum(is_all_discounted(want, number, discount[day:day + 10]) for day in range(len(discount)))
 
 
-def is_products_discounted(want, number, discount) -> bool:
-    return all(is_product_discounted(product, quantity, discount) for product, quantity in zip(want, number))
-
-
-def is_product_discounted(product, quantity, discount) -> bool:
-    return discount.count(product) >= quantity
+def is_all_discounted(want, number, discount) -> bool:
+    return all(discount.count(product) >= quantity for product, quantity in zip(want, number))
