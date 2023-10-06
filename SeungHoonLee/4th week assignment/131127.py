892fedd01fc097f88059f3588952b48d980bc095
LEE-sh1673 <lsh901673@gmail.com>
2023-10-06 15:59:03 +0900

    refactor: 할인 행사(131127) 메서드 수정
    - 사용되지 않은 매개변수 삭제

--- SeungHoonLee/4th week assignment/131127.py
+++ SeungHoonLee/4th week assignment/131127.py
@@ -13,10 +13,10 @@ LV2. 할인 행사
 
 
 def solution(want, number, discount) -> bool:
-    return sum(is_products_discounted(day, want, number, discount[day:day + 10]) for day in range(len(discount)))
+    return sum(is_products_discounted(want, number, discount[day:day + 10]) for day in range(len(discount)))
 
 
-def is_products_discounted(day, want, number, discount) -> bool:
+def is_products_discounted(want, number, discount) -> bool:
     return all(is_product_discounted(product, quantity, discount) for product, quantity in zip(want, number))
 
 
