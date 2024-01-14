847d90044e4fbb35de2c364f14bd6a459bb4e941
jiyeon <sjy6736@naver.com>
2024-01-14 22:37:54 +0900

    수식최대화.py 수정

--- JiYeonShin/13th week assignment/수식최대화.py
+++ JiYeonShin/13th week assignment/수식최대화.py
@@ -1,28 +1,60 @@
-#1번과 연결된 도시를 파고들면서 각 마을에 도착하는 최소시간을 구한다.
+#표현식 안에있는 연산자 찾아내고, 순열로 우선순위 리스트를 만든다.
+#우선순위 리스트를 돌면서 해당 우선순위로 표현식을 계산한다.
 
-#1번 도시와 연결된 2번 도시가 있다고 할 때,
-#IF 현재 2번 도시까지 걸리는 시간 > 1번 도시까지 걸리는 시간 + 1번도시 에서 2번도시까지 가는데 걸리는 시간 이라면 
-#2번 도시까지의 최소시간이 갱신되는것 -> 갱신하고 다시 2번 도시를 큐에 넣어서 2번을 거쳐갈 다른 도시들도 갱신하도록 한다.
+#우선순위로 표현식을 계산하는 로직
+#우선순위 : (*, +, -), 표현식 : 100 - 200 * 300 - 500 + 20 라고 할 떄
+#계산 전 표현식이 담긴 expressiondq, 특정 연산자 계산이 끝난 expressiondq_next
+#1. while(expressiondq) 돌면서
+#2. expression = expressiondq.popleft()
+#3. expression이 현재 계산해야할 연산자가 아니라면 expressiondq_next에 넣기
+#4. expression이 현재 계산해야할 연산자일 경우 expressiondq_next의 마지막에 있는 수와 expressiondq의 앞에 있는 수를 가지고 계산 후 expressiondq_next에 넣기
+#5. expressiondq = expressiondq_next
+#위 1234과정을 우선순위 : (*, +, -) 다 진행해준다.
 
+
+import copy
 from collections import deque
+import itertools
 
-def solution(N, road, K):
+def solution(expression):
     answer = 0
-    deliveryTimes = [0, 0] + [500000]*(N-1)
-    #그래프 만들기
-    graph =  [[] for i in range(N+1)]
-    for edge1, edge2, time in road:
-        graph[edge1].append([edge2, time])
-        graph[edge2].append([edge1, time])
-    dq = deque()
-    dq.append(1)
-    while(dq):
-        city = dq.popleft()
-        for connectCity, time in graph[city]:
-            if(deliveryTimes[connectCity] >= deliveryTimes[city] + time):
-                deliveryTimes[connectCity] = deliveryTimes[city] + time
-                dq.append(connectCity)
-    for i in deliveryTimes[1:]:
-        if(i <= K):
-            answer += 1
+    expressiondq = deque()
+    operationSet = set()
+    #연산자 종류 확인 & 분리작업
+    temp = ""
+    for i in expression:
+        if(i.isdigit()):
+            temp += i
+        else:
+            operationSet.add(i)
+            expressiondq.append(int(temp))
+            expressiondq.append(i)
+            temp = ""
+    expressiondq.append(int(temp))
+    #우선순위 순열 구하기 & 해당 우선순위로 계산하기
+    prioritys = itertools.permutations(operationSet, len(operationSet))
+    for priority in prioritys:
+        answer = max(answer, abs(calExpressionWithPriority(priority, expressiondq)))
     return answer
+
+def calExpressionWithPriority(priority, expressiondq):
+    expressiondq = copy.deepcopy(expressiondq)
+    for operation in priority:
+        expressiondq_next = deque()
+        while(expressiondq):
+            expression = expressiondq.popleft()
+            if(expression != operation):
+                expressiondq_next.append(expression)
+            else:
+                expressiondq_next.append(cal(expressiondq_next.pop(), expression, expressiondq.popleft()))
+        expressiondq = expressiondq_next
+    return expressiondq[0]
+    
+def cal(num1, operation, num2):
+    if(operation == "+"):
+        return num1 + num2
+    elif(operation == "-"):
+        return num1 - num2
+    elif(operation == "*"):
+        return num1*num2
+
